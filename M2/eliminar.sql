b4d8d8376abac7e98f50112cc48c4678eb8592a2
ihernandezvelarde <91269912+ihernandezvelarde@users.noreply.github.com>
2022-05-09 09:18:44 +0200

    Add drop procedure

---
+++ M2/eliminar.sql
@@ -0,0 +1,39 @@
+create or replace procedure drop_table as
+eliminar varchar(255);
+
+begin
+eliminar := '
+drop table battle_has_ships purge';
+execute immediate eliminar;
+
+eliminar := '
+drop table BATTLE_HAS_DEFENSES purge';
+execute immediate eliminar;
+
+eliminar := '
+drop table BATTLE purge';
+execute immediate eliminar;
+
+eliminar := '
+drop table PLANET_HAS_SHIPS purge';
+execute immediate eliminar;
+
+eliminar := '
+drop table PLANET_HAS_DEFENSES purge';
+execute immediate eliminar;
+
+eliminar := '
+drop table PLANET purge';
+execute immediate eliminar;
+
+eliminar := '
+drop table DEFENSE purge';
+execute immediate eliminar;
+
+eliminar := '
+drop table SHIP purge';
+execute immediate eliminar;
+eliminar := '
+drop table USU purge';
+execute immediate eliminar;
+end;
