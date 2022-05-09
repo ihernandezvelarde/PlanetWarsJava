2a19fd13067efb507380bbec23759ac769f4e1d4
ihernandezvelarde <91269912+ihernandezvelarde@users.noreply.github.com>
2022-05-09 12:47:12 +0200

    Update getDefense procedure

--- M2/getDefense.sql
+++ M2/getDefense.sql
@@ -1,13 +1,5 @@
-create or replace procedure getDefense(id_nave_defensa in number, nombre in out varchar, metal in out number, cristal in out number, deuterium in out number, inicial in out number, armadura in out number, base in out number, velocidad in out number, generar in out number)
+create or replace procedure getDefense(id_nave_defensa in out number, nombre in out varchar, metal in out number, cristal in out number, deuterium in out number, inicial in out number, armadura in out number, base in out number, velocidad in out number, generar in out number)
 as
 begin
-select defense_name into nombre from ship where id_defense = id_nave_defensa;
-select metal_cost into metal from ship where id_defense = id_nave_defensa;
-select crystal_cost into cristal from ship where id_defense = id_nave_defensa;
-select deuterium_cost into deuterium from ship where id_defense = id_nave_defensa;
-select initial_armor into inicial from ship where id_defense = id_nave_defensa;
-select armor into armadura from ship where id_defense = id_nave_defensa;
-select basedamage into base from ship where id_defense = id_nave_defensa;
-select speed into velocidad from ship where id_defense = id_nave_defensa;
-select generate_wastings into generar from ship where id_defense = id_nave_defensa;
+select * into id_nave_defensa,nombre,metal,cristal,deuterium,inicial,armadura,base,velocidad,generar from defense where id_defense = id_nave_defensa;
 end;
