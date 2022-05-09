533658a74d8ce5317c857e68d1796d0cce3bf5bb
itsJRillo <jmorillolabour@gmail.com>
2022-05-09 09:48:09 +0200

    Update IonCannon.java

--- M3/IonCannon.java
+++ M3/IonCannon.java
@@ -30,7 +30,7 @@ public class IonCannon extends Defense implements MilitaryUnit{
     public int getChanceAttackAgain() {return this.CHANCE_ATTACK_AGAIN_IONCANNON;}
 
     public void changeStatsByTech(int techArmor,int techDamage){
-        this.setBaseDamage(BASE_DAMAGE_MISSILELAUNCHER + (techDamage * PLUS_ATTACK_MISSILELAUNCHER_BY_TECHNOLOGY)*BASE_DAMAGE_MISSILELAUNCHER/100);
-        this.setInitialArmor(ARMOR_MISSILELAUNCHER + (techArmor * PLUS_ARMOR_MISSILELAUNCHER_BY_TECHNOLOGY)*ARMOR_MISSILELAUNCHER/100);
+        this.setBaseDamage(BASE_DAMAGE_IONCANNON + (techDamage * PLUS_ATTACK_IONCANNON_BY_TECHNOLOGY)*BASE_DAMAGE_IONCANNON/100);
+        this.setInitialArmor(ARMOR_IONCANNON + (techArmor * PLUS_ARMOR_IONCANNON_BY_TECHNOLOGY)*ARMOR_IONCANNON/100);
     }
 }
