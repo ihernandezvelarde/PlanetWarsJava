6f064adb8fdceb057f77e4d5cc8e4f67f874c72a
itsJRillo <jmorillolabour@gmail.com>
2022-05-11 10:47:30 +0200

    Conexión BBDD MissileLauncher - 11/05/22

--- M3/MissileLauncher.java
+++ M3/MissileLauncher.java
@@ -1,42 +1,51 @@
 package PlanetWars;
 
-import java.util.ArrayList;
+import java.sql.CallableStatement;
+import java.sql.Connection;
+import java.sql.SQLException;
 
 public class MissileLauncher extends Defense implements MilitaryUnit{
+    CallableStatement cst;
 
-    MissileLauncher() {
-        super.setInitialArmor(ARMOR_MISSILELAUNCHER);
-        super.setBaseDamage(BASE_DAMAGE_MISSILELAUNCHER);
-        super.setArmor(ARMOR_MISSILELAUNCHER);
+    MissileLauncher(int tecnoDefense, int tecnoAtack,Connection con) {
+        InfoDefense def=new InfoDefense();
+
+        cst = def.getInfoDefense(con,1);
+
+        try {
+			cst.execute();
+            super.setInitialArmor(cst.getInt(6));
+            super.setBaseDamage(cst.getInt(8));
+            this.setArmor(this.getInitialArmor() + (tecnoDefense*PLUS_ARMOR_MISSILELAUNCHER_BY_TECHNOLOGY)*this.getInitialArmor()/100);
+            this.setBaseDamage(this.getBaseDamage()+(tecnoAtack*PLUS_ATTACK_MISSILELAUNCHER_BY_TECHNOLOGY)*this.getBaseDamage()/100);
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
     }
 
     @Override
     public int attack() {return this.getBaseDamage();}
 
     @Override
-    public void takeDamage(int receivedDamage) {
-        this.setArmor(this.getActualArmor() - receivedDamage);
-    }
+    public void takeDamage(int receivedDamage) {this.setArmor(this.getActualArmor() - receivedDamage);}
 
     @Override
     public int getActualArmor() {return this.getArmor();}
 
     @Override
-    public int getMetalCost() {return this.METAL_COST_MISSILELAUNCHER;}
-
-    @Override
-    public int getDeuteriumCost() {return this.DEUTERIUM_COST_MISSILELAUNCHER;}
-
-    @Override
-    public int getChanceGeneratinWaste() {return this.CHANCE_GENERATNG_WASTE_MISSILELAUNCHER;}
-
-    @Override
     public int getChanceAttackAgain() {return this.CHANCE_ATTACK_AGAIN_MISSILELAUNCHER;}
 
-    public void changeStatsByTech(int techArmor,int techDamage){
-        this.setBaseDamage(BASE_DAMAGE_MISSILELAUNCHER + (techDamage * PLUS_ATTACK_MISSILELAUNCHER_BY_TECHNOLOGY)*BASE_DAMAGE_MISSILELAUNCHER/100);
-        this.setInitialArmor(ARMOR_MISSILELAUNCHER + (techArmor * PLUS_ARMOR_MISSILELAUNCHER_BY_TECHNOLOGY)*ARMOR_MISSILELAUNCHER/100);
-    }
+	@Override
+	public int getMetalCost() {
+        try {return cst.getInt(3);}
+        catch (SQLException e) {return 0;}
+	}
+	@Override
+	public int getDeuteriumCost() {
+        try {return cst.getInt(5);}
+        catch (SQLException e) {return 0;}
+	}
+	@Override
+	public int getChanceGeneratinWaste() {return CHANCE_GENERATNG_WASTE_MISSILELAUNCHER;}
 }
-
-
