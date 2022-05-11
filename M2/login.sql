6de4c3ecf5c2d54a9a9ac8fe80f60f541e0bbd11
eriktamano <95917206+eriktamano@users.noreply.github.com>
2022-05-11 10:25:38 +0200

    Update fixed errors

--- M2/login.sql
+++ M2/login.sql
@@ -1,7 +1,7 @@
-create or replace function login(nombre in varchar, contrasenya in varchar) return boolean as
+create or replace function login(nombre in varchar, contr in varchar) return boolean as
 cod number;
 begin
-select id_user into cod from usu where username = nombre and password = contrasenya;
+select id_user into cod from usu where username = nombre and contrasenya = contr;
 return true;
 exception
 when no_data_found then
