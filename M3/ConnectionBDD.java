ffb16cdf124473bbc58875ecb739d97e731bad2c
itsJRillo <jmorillolabour@gmail.com>
2022-05-13 11:48:27 +0200

    Update ConnectionBDD.java

--- M3/ConnectionBDD.java
+++ M3/ConnectionBDD.java
@@ -9,20 +9,20 @@ public class ConnectionBDD {
 	String password;
 	public ConnectionBDD() {
 		// Carga el driver de oracle
-       try {
-		DriverManager.registerDriver(new oracle.jdbc.driver.OracleDriver());
+		try {
+			DriverManager.registerDriver(new oracle.jdbc.driver.OracleDriver());
 
-		// Conecta con la base de datos orcl con el usuario system y la contrasea password
-        conex = DriverManager.getConnection("jdbc:oracle:thin:@192.168.40.2:1521:orcl", "alumnoAMS15", "alumnoAMS15");
-		System.out.println("Conectado MAQUINAAA");
+			// Conecta con la base de datos orcl con el usuario system y la contrasea password
+			conex = DriverManager.getConnection("jdbc:oracle:thin:@192.168.40.2:1521:orcl", "alumnoAMS15", "alumnoAMS15");
+			System.out.println("Conectado MAQUINAAA");
 		}
 
-	   catch (SQLException e) {System.out.println("Error de conexion");}
+		catch (SQLException e) {System.out.println("Error de conexion");}
 	}
 	public Connection getConex() {
 		return conex;
 	}
-	
+
 }
 
 class InfoShips  {
@@ -31,29 +31,52 @@ class InfoShips  {
 		try {
 			cst = con.prepareCall("{call getship (?,?,?,?,?,?,?,?,?,?)}");
 
-			 // Parametro 1 del procedimiento almacenado
-            cst.setInt(1, id_nave);
-            
-            // Definimos los tipos de los parametros de salida del procedimiento almacenado
-            cst.registerOutParameter(2, java.sql.Types.VARCHAR);
-            cst.registerOutParameter(3, java.sql.Types.INTEGER);
-            cst.registerOutParameter(4, java.sql.Types.INTEGER);
-            cst.registerOutParameter(5, java.sql.Types.INTEGER);
-            cst.registerOutParameter(6, java.sql.Types.INTEGER);
-            cst.registerOutParameter(7, java.sql.Types.INTEGER);
-            cst.registerOutParameter(8, java.sql.Types.INTEGER);
-            cst.registerOutParameter(9, java.sql.Types.INTEGER);
-            cst.registerOutParameter(10, java.sql.Types.INTEGER); 
+			// Parametro 1 del procedimiento almacenado
+			cst.setInt(1, id_nave);
+
+			// Definimos los tipos de los parametros de salida del procedimiento almacenado
+			cst.registerOutParameter(2, java.sql.Types.VARCHAR);//nombre
+			cst.registerOutParameter(3, java.sql.Types.INTEGER);//metal cost
+			cst.registerOutParameter(4, java.sql.Types.INTEGER);//cristal
+			cst.registerOutParameter(5, java.sql.Types.INTEGER);//deuterium
+			cst.registerOutParameter(6, java.sql.Types.INTEGER);//initial armor
+			cst.registerOutParameter(7, java.sql.Types.INTEGER);//armor
+			cst.registerOutParameter(8, java.sql.Types.INTEGER);//base dmg
+			cst.registerOutParameter(9, java.sql.Types.INTEGER);// speed
+			cst.registerOutParameter(10, java.sql.Types.INTEGER); // wastings
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 
-	return cst;
+		return cst;
 	}
-	
-}
 
+}
+class InfoPlanet {
+	CallableStatement getInfoPlanet(Connection con, int id_planet) {
+		CallableStatement cst = null;
+		try {
+			cst = con.prepareCall("{call get_planet (?,?,?,?,?,?,?,?)}");
+			// Parametro 1 del procedimiento almacenado
+			cst.setInt(1, id_planet);
+
+			// Definimos los tipos de los parametros de salida del procedimiento almacenado
+			cst.registerOutParameter(2, java.sql.Types.VARCHAR);//name
+			cst.registerOutParameter(3, java.sql.Types.INTEGER);//tech atk
+			cst.registerOutParameter(4, java.sql.Types.INTEGER);//tech def
+			cst.registerOutParameter(5, java.sql.Types.INTEGER);//cristal
+			cst.registerOutParameter(6, java.sql.Types.INTEGER);//metal
+			cst.registerOutParameter(7, java.sql.Types.INTEGER);//deuterio
+			cst.registerOutParameter(8, java.sql.Types.INTEGER);//id user
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+
+		return cst;
+
+	}
+}
 class InfoDefense {
 	CallableStatement getInfoDefense(Connection con, int id_nave) {
 		CallableStatement cst = null;
@@ -127,5 +150,60 @@ class InfoUsers{
 			cst.execute();
 		}
 		catch (SQLException e) {e.printStackTrace();}
+	}
+	int getIdUser(Connection con, String name){
+
+		CallableStatement cst = null;
+
+		try {
+			cst = con.prepareCall("{? = call getid (?) }");
+			cst.registerOutParameter(1,Types.INTEGER);
+			cst.setString(2,name);
+			cst.execute();
+		}
+		catch (SQLException e) {e.printStackTrace();}
+
+		try {return cst.getInt(1);}
+		catch (SQLException e){e.printStackTrace();}
+		return 0;
+	}
+
+	void anyadePlaneta(Connection con, String name,int id_user) {
+		CallableStatement cst = null;
+
+		try {
+			cst = con.prepareCall("{call add_planet (?,?,?,?,?,?,?) }");
+			cst.setString(1,name);
+			cst.setInt(2,0);
+			cst.setInt(3,0);
+			cst.setInt(4,0);
+			cst.setInt(5,0);
+			cst.setInt(6,0);
+			cst.setInt(7, id_user);
+			cst.execute();
+		}
+		catch (SQLException e) {e.printStackTrace();}
+	}
+	CallableStatement getInfoPlanetId(Connection con, String usuario) {
+		CallableStatement cst = null;
+		try {
+			cst = con.prepareCall("{call get_planetusu (?,?) }");
+
+			// Parametro 1 del procedimiento almacenado
+			cst.setString(1, usuario);
+
+			// Definimos los tipos de los parametros de salida del procedimiento almacenado
+
+			cst.registerOutParameter(2, java.sql.Types.INTEGER);
+
+
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+
+		return cst;
+
 	}
 }
