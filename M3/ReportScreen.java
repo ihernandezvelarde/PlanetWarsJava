1488f70d987b656593849c1713b0f35bc146591a
eriktamano <95917206+eriktamano@users.noreply.github.com>
2022-05-17 01:07:20 +0200

    Update finished, havent tested

--- M3/ReportScreen.java
+++ M3/ReportScreen.java
@@ -5,16 +5,19 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.sql.CallableStatement;
 import java.sql.Connection;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
 
-import com.mysql.cj.ParseInfo;
+
 
 public class ReportScreen extends JPanel {
 	
@@ -30,6 +33,8 @@ public class ReportScreen extends JPanel {
 	planet_metal_costs, planet_deuterium_costs, enemy_metal_costs, enemy_deuterium_costs, planet_metal_losses, 
 	planet_deuterium_losses, enemy_metal_losses, enemy_deuterium_losses;
 	
+	String complete_rep;
+	
 	int[] starting_units_planet = new int[7];
 	int[] final_units_planet = new int[7];
 	
@@ -38,37 +43,85 @@ public class ReportScreen extends JPanel {
 	
 	public ReportScreen(Connection con) {
 		
+		// guarda la connexio a la bbdd com a un atribut
 		this.con = con;
 		
+		// panel principal que conte tots els elements i els altres panels
 		panel1 = new JPanel();
 		panel1.setLayout(new GridLayout(2, 1));
 		
+		// panel que conte la JLabel de id de batalla, un textfield per a insertar la id de batalla i un panel amb dos botons per a mostrar els reports
 		panel2 = new JPanel();
 		panel2.setLayout(new GridLayout(1, 3));
 		
+		// panel que conte els botons per a mostrar els reports
 		panel3 = new JPanel();
 		panel3.setLayout(new GridLayout(2, 1));
 		
+		// textfield per a insertar la id de batalla
 		id_bat = new JTextField();
 		id_bat.setSize(100, 100);
 		
+		// textarea per a mostrar els reports
 		terminal = new JTextArea();
 		terminal.setEditable(false);
 		
+		// botons per a mostrar els reports i els seus respectius listeners
 		boton_resumen = new JButton("Show Battle Summary");
 		boton_resumen.addActionListener(new ActionListener() {
 
 			@Override
 			public void actionPerformed(ActionEvent arg0) {
-				getVariables(Integer.parseInt(id_bat.getText()));
-				terminal.setText(getSummary(Integer.parseInt(id_bat.getText())));
+				try {
+					// primer es comprova que la ID sigui correcta
+					CallableStatement cst = con.prepareCall("{call check_battle (?, ?)");
+					cst.setInt(1, Integer.parseInt(id_bat.getText()));
+					cst.registerOutParameter(2, java.sql.Types.INTEGER);
+					if (cst.getInt(2) == 0) {
+						JOptionPane.showMessageDialog(null, "ERROR: battle ID does not exist");;
+					} else {
+						// si existeix la batalla es genera el report resumit
+						setVariables(Integer.parseInt(id_bat.getText()));
+						terminal.setText(getSummary(Integer.parseInt(id_bat.getText())));
+					}
+				} catch (SQLException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (NumberFormatException e) {
+					JOptionPane.showMessageDialog(null, "ERROR: battle ID must be a number");
+				}
 			}
 			
 		});
 		
 		boton_completo = new JButton("Show Battle Development");
+		boton_completo.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				try {
+					CallableStatement cst = con.prepareCall("{call check_battle (?, ?)");
+					cst.setInt(1, Integer.parseInt(id_bat.getText()));
+					cst.registerOutParameter(2, java.sql.Types.INTEGER);
+					if (cst.getInt(2) == 0) {
+						JOptionPane.showMessageDialog(null, "ERROR: battle ID does not exist");;
+					} else {
+						setVariables(Integer.parseInt(id_bat.getText()));
+						setCompleteReport(Integer.parseInt(id_bat.getText()));
+						terminal.setText(complete_rep);
+					}
+					
+				} catch (SQLException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				} catch (NumberFormatException e1) {
+					JOptionPane.showMessageDialog(null, "ERROR: battle ID must be a number");
+				}
+			}
+			
+		});
 		
-		
+		// s'aniden els panels i s'afageixen a la finestra
 		panel1.add(panel2);
 		panel1.add(terminal);
 		panel2.add(label_bat);
@@ -109,45 +162,46 @@ public class ReportScreen extends JPanel {
 	
 	private void setUnits(int id_battle, int index) {
 		try {
-			CallableStatement cst =  con.prepareCall("{call get_ship_battle (?, ?, ?, ?, ?)}");
+			CallableStatement cst1 = con.prepareCall("{call get_ship_battle (?, ?, ?, ?, ?)}");
+			CallableStatement cst2 = con.prepareCall("{call get_defense_battle (?, ?, ?, ?, ?)}");
 			
-			cst.setInt(1, id_battle);
+			cst1.setInt(1, id_battle);
 			
-			cst.registerOutParameter(4, java.sql.Types.INTEGER);
-			cst.registerOutParameter(5, java.sql.Types.INTEGER);
+			cst1.registerOutParameter(4, java.sql.Types.INTEGER);
+			cst1.registerOutParameter(5, java.sql.Types.INTEGER);
 			
-			if (index > 3) {
-				cst.setInt(2, index-3);
-				cst.setInt(3, 0);
-				
-				cst.execute();
-				
-				starting_units_planet[index] = cst.getInt(4);
-				final_units_planet[index] = cst.getInt(5);
-				
-			} else {
-				cst.setInt(2, index+1);
-				cst.setInt(3, 0);
-				
-				cst.execute();
-				
-				starting_units_planet[index] = cst.getInt(4);
-				final_units_planet[index] = cst.getInt(5);
-				
-				cst.setInt(3, 1);
-				
-				cst.execute();
-				
-				starting_units_enemy[index] = cst.getInt(4);
-				final_units_enemy[index] = cst.getInt(5);
+			cst1.execute();
+			
+			cst1.setInt(2, index+1);
+			cst1.setInt(3, 0);
+			
+			cst1.execute();
+			
+			starting_units_planet[index] = cst1.getInt(4);
+			final_units_planet[index] = cst1.getInt(5);
+			
+			cst1.setInt(3, 1);
+			
+			cst1.execute();
+			
+			starting_units_enemy[index] = cst1.getInt(4);
+			final_units_enemy[index] = cst1.getInt(5);
+			
+			if (index < 3) {
+				cst2.setInt(1, id_battle);
+				cst2.setInt(2, index+1);
+				cst2.setInt(3, 0);
+				starting_units_planet[index+4] = cst2.getInt(4);
+				final_units_planet[index+4] = cst2.getInt(4);
 			}
+			
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
 	
-	private void getVariables(int id_batalla) {
+	private void setVariables(int id_batalla) {
 		try {
 			CallableStatement cst = con.prepareCall("{call SHOW_REPORT (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)}");
 			cst.setInt(1, id_batalla);
@@ -171,6 +225,10 @@ public class ReportScreen extends JPanel {
 			enemy_metal_losses = cst.getInt(12);
 			enemy_deuterium_losses = cst.getInt(13);
 			
+			for (int i = 0; i < 4; i++) {
+				setUnits(id_batalla, i);
+			}
+			
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -178,12 +236,29 @@ public class ReportScreen extends JPanel {
 		
 	}
 	
-	private void getCompleteReport(int id_batalla) {
+	private void setCompleteReport(int id_batalla) {
 		try {
+			CallableStatement cst = con.prepareCall("{call GET_STEP_AMOUNT (?, ?)}");
 			
-			CallableStatement cst = con.prepareCall("{call GETBATTLESTEP (?, ?, ?)");
 			cst.setInt(1, id_batalla);
-			cst.setInt(2, 2);
+			
+			cst.execute();
+			cst.registerOutParameter(2, java.sql.Types.INTEGER);
+			
+			String msg = "";
+			
+			int num_steps = cst.getInt(2);
+			for (int i = 1; i <= i; i++) {
+				cst = con.prepareCall("{call GETBATTLESTEP (?, ?, ?)}");
+				cst.setInt(1, id_batalla);
+				cst.setInt(2, i);
+				
+				cst.registerOutParameter(3, java.sql.Types.VARCHAR);
+				
+				msg += "\n" + cst.getString(3);
+			}
+			
+			complete_rep = msg;
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -191,7 +266,6 @@ public class ReportScreen extends JPanel {
 		
 	}
 }
-
 
 
 
