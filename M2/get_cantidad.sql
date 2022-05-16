df408f0e8f3bf03df807fba63c0587114d6950b0
ihernandezvelarde <91269912+ihernandezvelarde@users.noreply.github.com>
2022-05-16 13:01:34 +0200

    Update get_cantidad

--- M2/get_cantidad.sql
+++ M2/get_cantidad.sql
@@ -1,4 +1,4 @@
-create or replace procedure get_cantidad(ship_id in number,planet_id in number, nivel_ataque in number, nivel_defensa in number, cantidad in out number)
+create or replace procedure get_cantidadShip(ship_id in number,planet_id in number, nivel_ataque in number, nivel_defensa in number, cantidad in out number)
 as
 begin
 select cant into cantidad from planet_has_ships where id_ship = ship_id and id_planet = planet_id and Lvl_attack = nivel_ataque and Lvl_defense = nivel_defensa;
