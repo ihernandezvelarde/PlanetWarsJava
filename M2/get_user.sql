cada2e26fe0b991a89032afe803349c1ca18592b
eriktamano <95917206+eriktamano@users.noreply.github.com>
2022-05-19 01:31:02 +0200

    Delete M2 directory

--- M2/get_user.sql
+++
@@ -1,8 +0,0 @@
-create or replace procedure get_user(nombre_usu in out varchar, cod_usu out number, fecha_nac out date, contrasenya out varchar, existe out number) as
-begin
-select * into cod_usu, nombre_usu, fecha_nac, contrasenya from USU where username = nombre_usu;
-existe := 1;
-exception
-when no_data_found then
-existe := 0;
-end;
--- M2/login.sql
+++
@@ -1,9 +0,0 @@
-create or replace function login(nombre in varchar, contr in varchar) return boolean as
-cod number;
-begin
-select id_user into cod from usu where username = nombre and contrasenya = contr;
-return true;
-exception
-when no_data_found then
-return false;
-end;
